1ccc85ed04ead936e45f3dd68d29a4d7a36244b0
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2021-10-16 22:31:13 -0400

    Completed update note part

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -12,7 +12,7 @@ app.post('/note', async (req, res) => {
         await note.save()
         res.send(note)
         console.log("Note Saved.")
-        res.status(200).send("Noted Saved.")
+        res.status(200).send("Note Saved.")
     } catch(err) {
         console.log("ERROR: Note Not Saved " + err)
         res.status(500).send(err)
@@ -27,7 +27,7 @@ app.get('/notes', async (req, res) => {
 
     try {
         res.send(notes)
-    } catch (err) {
+    } catch(err) {
         console.log("ERROR: " + err)
         res.status(500).send(err)
     }
@@ -40,7 +40,7 @@ app.get('/notes/:noteId', async (req, res) => {
     try {
         const notesId = await noteModel.findById(req.params.noteId)
         res.send(notesId)
-    } catch (err) {
+    } catch(err) {
         console.log("ERROR: " + err)
         res.status(500).send(err)
     }
@@ -48,19 +48,23 @@ app.get('/notes/:noteId', async (req, res) => {
 
 // Update a Note with noteId
 // http://mongoosejs.com/docs/api.html#findbyidandupdate_findByIdAndUpdate
-app.put('/notes/:noteId', (req, res) => {
+app.put('/notes/:noteId', async (req, res) => {
     // Write your code here to update the note using noteid
+    try {
+        await noteModel.findByIdAndUpdate(req.params.noteId, req.body)
+        note = await noteModel.save()
+        res.send(note)
+        console.log("Note Updated.")
+        res.status(200).send("Note Updated.")
+    } catch(err) {
+        console.log("ERROR: Note Not Updated. " + err)
+        res.status(500).send(err)
+    }
 });
 
 //TODO - Delete a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandremove_findByIdAndRemove
 app.delete('/notes/:noteId', (req, res) => {
-    // Validate request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
-        });
-    }
     //TODO - Write your code here to delete the note using noteid
 });
 
